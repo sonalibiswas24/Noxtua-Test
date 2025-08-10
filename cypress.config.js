296eceb055fbf44f71e242c7cc3a7fc2371bd472
Sonali Biswas <sonalibiswas@Mac.fritz.box>
2025-08-10 21:09:05 +0200

    Fix GitHub Actions: remove hardcoded localhost, use Cypress built-in server management

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:3000', // This will be overridden in CI
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
